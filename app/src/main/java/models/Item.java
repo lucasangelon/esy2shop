61c3778c9ea067e6c8107e89ce230370276cf2f0
Urist McPCMR <jack.kitchener-work@mail.com>
2015-08-20 11:24:38 +0800

    Added List class

---
+++ app/src/main/java/models/Item.java
@@ -0,0 +1,69 @@
+package models;
+
+/**
+ * Item
+ *
+ * One item stored on A list
+ *
+ * Created by 041402465 on 20/08/2015.
+ */
+public class Item {
+
+    /*
+        Vars
+     */
+    private int id;
+    private String name;
+    private boolean complete;
+
+    /*
+        Constructors
+     */
+    public Item(){}
+
+    public Item(int id,String name){
+
+        this.id=id;
+        this.name=name;
+
+        /*
+            complate always defaults to false
+         */
+        complete = false;
+
+    }
+
+
+    /*
+        Get and Set
+     */
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+
+}
---
+++ app/src/main/java/models/List.java
@@ -0,0 +1,16 @@
+package models;
+
+/**
+ * List
+ *
+ * Stores a list of items
+ *
+ * Created by 041402465 on 20/08/2015.
+ */
+public class List{
+
+    /*
+        Vars
+     */
+
+}

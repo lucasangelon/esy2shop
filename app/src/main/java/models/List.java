22168cd6973ac49cb87ed03bc23477875be39f4e
Urist McPCMR <jack.kitchener-work@mail.com>
2015-08-20 13:05:59 +0800

    updated list and item

--- app/src/main/java/models/List.java
+++ app/src/main/java/models/List.java
@@ -1,5 +1,8 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * List
  *
@@ -12,5 +15,23 @@ public class List{
     /*
         Vars
      */
+    ArrayList<Item> items;
+    String name;
+    int catagory;
+    boolean complete;
+
+    List(String p_name, int p_catagory){
+        items = new ArrayList<Item>();
+        name = p_name;
+        catagory = p_catagory;
+    }
+
+    public void add(Item item){
+        items.add(item);
+    }
+
+    public void remove(int index){
+        items.remove(index);
+    }
 
 }

5d4cf451bdad78ce09e169e36f9cdcc1a07f5b1e
james112358 <041401076@my.central.wa.edu.au>
2015-08-27 14:56:29 +0800

    Re-organized package structure
    Added many packages to help with organisation of the files and classes

---
+++ app/src/main/java/codefactory/projectshop/models/List.java
@@ -0,0 +1,125 @@
+package codefactory.projectshop.models;
+
+import java.util.ArrayList;
+
+/**
+ * List
+ *
+ * Stores a list of items
+ *
+ * Created by 041402465 on 20/08/2015.
+ */
+public class List{
+
+    /*
+        Vars
+     */
+    ArrayList<Item> items;
+    String name;
+    String catagory;
+    boolean complete;
+    int storeId;
+
+
+    /*
+        Constructors
+     */
+    public List(){
+
+        items = new ArrayList<Item>();
+        complete = false;
+
+    }
+
+
+    public List(String name, String catagory){
+        items = new ArrayList<Item>();
+        this.name = name;
+        this.catagory = catagory;
+        complete = false;
+    }
+
+
+    /*
+        Methods
+     */
+
+    /**
+     * Adds an Item to the arraylist
+     * @param item
+     */
+    public void add(Item item){
+        items.add(item);
+    }
+
+    /**
+     * Removes the Item at the specified index
+     * @param index
+     */
+    public void remove(int index){
+        items.remove(index);
+    }
+
+    /**
+     * Returns the size of the list
+     * @return int
+     */
+    public int listSize(){
+        return items.size();
+    }
+
+    /**
+     * Returns a single item
+     *
+     * @param index
+     * @return Item
+     */
+    public Item getItem(int index){
+        return items.get(index);
+    }
+
+    /*
+        Getters and Setters
+     */
+    public String getCatagory() {
+        return catagory;
+    }
+
+    public int getStoreId() {
+        return storeId;
+    }
+
+    public ArrayList<Item> getItemList() {
+        return items;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    //--------------------------
+
+    public void setStoreId(int storeId) {
+        this.storeId = storeId;
+    }
+
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
+    }
+
+    public void setComplete(boolean complete) {
+        this.complete = complete;
+    }
+
+    public void setCatagory(String catagory) {
+        this.catagory = catagory;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/codefactory/projectshop/models/Store.java
@@ -0,0 +1,76 @@
+package codefactory.projectshop.models;
+
+/**
+ * Store
+ *
+ * Saves the store assicitated with a list
+ *
+ * Created by 041401076 on 20/08/2015.
+ */
+public class Store {
+
+    /*
+        Vars
+     */
+
+    private String name;
+    private int id;
+    private double latitude;
+    private double longitude;
+
+
+    /*
+        Constructors
+     */
+
+    public Store(){}
+
+    public Store(int id, String name, double longitude,double latitude){
+
+        this.id=id;
+        this.name=name;
+        this.longitude=longitude;
+        this.latitude=latitude;
+
+    }
+
+    /*
+        Get and Set
+     */
+
+    public String getName() {
+        return name;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+
+    
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}

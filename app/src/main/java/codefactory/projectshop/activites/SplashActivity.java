0ee679f991d18775fadbe9180ac6ec1719b9fe44
james112358 <041401076@my.central.wa.edu.au>
2015-09-09 11:15:56 +0800

    Checks GPS and Network

--- app/src/main/java/codefactory/projectshop/activites/SplashActivity.java
+++ app/src/main/java/codefactory/projectshop/activites/SplashActivity.java
@@ -1,6 +1,12 @@
 package codefactory.projectshop.activites;
 
+import android.app.AlertDialog;
+import android.app.Application;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -15,6 +21,34 @@ public class SplashActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        /*
+            Test Location and network Services
+         */
+        LocationManager lm = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+        boolean gps_enabled = false;
+        boolean network_enabled = false;
+
+        //GPS
+        try {
+
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        }catch(Exception genericEx){} // I know catching generics is bad, lay off me im tired
+
+        //Network
+        try{
+
+            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        } catch(Exception genericEx){}
+
+
+        /*
+            Show dialog not enabled
+         */
+        if (!gps_enabled && network_enabled) locationPrompt();
 
 
 
@@ -51,4 +85,53 @@ public class SplashActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+
+
+    /*
+        Prompts user to turn on Location/Network services
+     */
+    public void locationPrompt(){
+
+            final Context context = getApplicationContext();
+            AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+            dialog.setMessage("Please Enable Location Services");
+
+            /*
+                User taken to location settings
+             */
+            dialog.setPositiveButton(context.getResources().getString(R.string.open_location_settings), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+
+                    Intent myIntent = new Intent( Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    context.startActivity(myIntent);
+
+                }
+            });
+
+
+            /*
+                User Exits application instead of turning on location
+                (no fun)
+             */
+            dialog.setNegativeButton(context.getString(R.string.cancel), new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+
+                    Intent intent = new Intent(Intent.ACTION_MAIN);
+                    intent.addCategory(Intent.CATEGORY_HOME);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+
+                }
+            });
+
+
+            dialog.show();
+        }
+
+
+    }
+
+

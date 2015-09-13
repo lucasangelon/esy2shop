01be8712d043a197eafc4d9c0096e59b6b59c309
Josh <wozaski@gmail.com>
2015-09-13 23:08:22 +0800

    MainMenu added
    MainMenu layout + corresponding java added
    Test list of list with ArrayAdapter added

---
+++ app/src/main/java/codefactory/projectshop/activites/MainMenu.java
@@ -0,0 +1,29 @@
+package codefactory.projectshop.activites;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+
+import codefactory.projectshop.R;
+
+public class MainMenu extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+
+        //Test for now, will change to get list of lists later
+        String[] listOfListsTest = {"Test 1", "Test 2", "Test 3", "Test 4", "Test 5", "Test 6", "Test 7"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, listOfListsTest);
+        setListAdapter(adapter);
+
+    }
+}
+

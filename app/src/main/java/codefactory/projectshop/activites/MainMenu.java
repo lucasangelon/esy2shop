dd9143118b62e3e32a0128644f74377d6e79f2b1
Josh <wozaski@gmail.com>
2015-09-15 00:39:24 +0800

    Added Image Resource for settings fragment
    Still attempting to implement it, can't get it to show on main screen
    just yet.

--- app/src/main/java/codefactory/projectshop/activites/MainMenu.java
+++ app/src/main/java/codefactory/projectshop/activites/MainMenu.java
@@ -1,12 +1,15 @@
 package codefactory.projectshop.activites;
 
 import android.app.ListActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
+import android.app.ActionBar;
+
 
 import codefactory.projectshop.R;
 
@@ -17,7 +20,6 @@ public class MainMenu extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-
         //Test for now, will change to get list of lists later
         String[] listOfListsTest = {"Test 1", "Test 2", "Test 3", "Test 4", "Test 5", "Test 6", "Test 7"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
@@ -25,5 +27,12 @@ public class MainMenu extends ListActivity {
         setListAdapter(adapter);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
 }
 

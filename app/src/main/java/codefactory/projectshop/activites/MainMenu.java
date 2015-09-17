2a0830acce3f6360c7bb9d36d26b157f4dd5125f
Josh <wozaski@gmail.com>
2015-09-17 15:13:12 +0800

    Gives a unique list now! :')
    Errors on delete button still will fix tomorrow

--- app/src/main/java/codefactory/projectshop/activites/MainMenu.java
+++ app/src/main/java/codefactory/projectshop/activites/MainMenu.java
@@ -6,25 +6,59 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.app.ActionBar;
+import android.widget.ListView;
 
+
+import junit.framework.Test;
+
+import java.util.ArrayList;
 
 import codefactory.projectshop.R;
+import codefactory.projectshop.models.Item;
+import codefactory.projectshop.models.List;
 
-public class MainMenu extends ListActivity {
+public class MainMenu extends AppCompatActivity {
+
+    ListView mainListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        //Test for now, will change to get list of lists later
+        //Array for adapter
+        ArrayList<List> listOfLists = new ArrayList<>();
+
+
+        //setting up default data
+        List testList1 = new List("Groceries", "1");
+        listOfLists.add(testList1);
+        List testList2 = new List("Pokemon", "2");
+        listOfLists.add(testList2);
+        List testList3 = new List("Digimon", "3");
+        listOfLists.add(testList3);
+        List testList4 = new List("Tamagotchis", "1");
+        listOfLists.add(testList4);
+
+
+        //Set adapter to list view
+        ListAdapter adapter = new codefactory.projectshop.adapters.ListAdapter(listOfLists, MainMenu.this);
+        mainListView = (ListView)findViewById(R.id.mainListView);
+        mainListView.setAdapter(adapter);
+
+
+
+
+      /*  Test for now, will change to get list of lists later
         String[] listOfListsTest = {"Test 1", "Test 2", "Test 3", "Test 4", "Test 5", "Test 6", "Test 7"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, listOfListsTest);
-        setListAdapter(adapter);
+                android.R.layout.simple_list_item_1, );
+        setListAdapter(adapter); */
 
     }
 

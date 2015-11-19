19a02d77a6315c8ee4d680918c3e7d3811afde5f
james112358 <041401076@my.central.wa.edu.au>
2015-11-19 08:37:38 +0800

    Commit After Tafe 18/11

--- app/src/main/java/codefactory/esy2shop/activites/Notification.java
+++ app/src/main/java/codefactory/esy2shop/activites/Notification.java
@@ -21,6 +21,7 @@ import codefactory.projectshop.R;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.location.Geofence;
 
 import java.util.Calendar;
 
@@ -28,10 +29,18 @@ public class Notification extends Activity {
 
 
 
-    private Spinner dateSpinner;
-    private Spinner timeSpinner;
+    Spinner dateSpinner;
+    Spinner timeSpinner;
     private static Calendar calendar;
-    private Address locationNotification;
+
+    //Used to set the date in the spinners
+    private Calendar tempCalendar;
+    private int tempInt;
+
+
+    //Geofencing variables
+
+    Address locationNotification;
 
 
 
@@ -54,14 +63,12 @@ public class Notification extends Activity {
 
         //Calender Being init
         calendar = Calendar.getInstance();
-
-
         /*
             Gets intent Extras
          */
         //If store has been selected on the maps activity
-        //if(getIntent().getExtras().containsKey("storeAddress"))
-            //locationNotification = getIntent().getExtras().getParcelable("storeAddress");
+        if(getIntent().getExtras().containsKey("storeAddress"))
+            locationNotification = getIntent().getExtras().getParcelable("storeAddress");
 
 
 
@@ -87,24 +94,33 @@ public class Notification extends Activity {
                 switch (position) {
                     //Today
                     case 0:
-                        Toast.makeText(parent.getContext(), "Spinner item 1!", Toast.LENGTH_SHORT).show();
+
+                        //Sets the calenders current date to today
+                        tempCalendar = Calendar.getInstance();
+                        calendar.set(tempCalendar.get(Calendar.YEAR),tempCalendar.get(Calendar.MONTH),tempCalendar.get(Calendar.DAY_OF_MONTH));
                         break;
 
                     //Tommorow
                     case 1:
-                        Toast.makeText(parent.getContext(), "Spinner item 2!", Toast.LENGTH_SHORT).show();
+
+                        tempCalendar = Calendar.getInstance();
+                        tempInt = tempCalendar.get(Calendar.DAY_OF_MONTH) + 1;
+                        calendar.set(Calendar.DAY_OF_MONTH, tempInt);
                         break;
 
                     //Next Week
                     case 2:
-                        Toast.makeText(parent.getContext(), "Spinner item 3!", Toast.LENGTH_SHORT).show();
+
+                        tempCalendar = Calendar.getInstance();
+                        tempInt = tempCalendar.get(Calendar.DAY_OF_MONTH) + 1;
+                        calendar.set(Calendar.DAY_OF_MONTH,tempInt);
                         break;
 
                     //Select a date
                     case 3:
+
                         DialogFragment newFragment = new DatePickerFragment();
-                        newFragment.show(getFragmentManager(),"DatePicker");
-                        Toast.makeText(parent.getContext(), "DateItem 4", Toast.LENGTH_SHORT).show();
+                        newFragment.show(getFragmentManager(), "DatePicker");
                         break;
 
                     default:
@@ -124,13 +140,17 @@ public class Notification extends Activity {
         //__________________________________________________________________
 
 
-        //Time Spinner
+        //Initisiling Time Spinner
         timeSpinner = (Spinner) findViewById(R.id.time_spinner);
         ArrayAdapter<CharSequence> timeOptionAdapter = ArrayAdapter.createFromResource(this,
                 R.array.time_options, android.R.layout.simple_spinner_item);
         timeOptionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         timeSpinner.setAdapter(timeOptionAdapter);
 
+
+        /*
+            Listener
+         */
         timeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -138,22 +158,33 @@ public class Notification extends Activity {
                 switch (position) {
                     //Morning is selected
                     case 0:
-                        Toast.makeText(parent.getContext(), "Spinner item 1!", Toast.LENGTH_SHORT).show();
+
+
+                        /*
+                            Sets time on calender to 9:00 am
+                         */
+                        calendar.set(Calendar.HOUR_OF_DAY, 9);
                         break;
 
                     //Noon is selected
                     case 1:
-                        Toast.makeText(parent.getContext(), "Spinner item 2!", Toast.LENGTH_SHORT).show();
+
+                        // Sets to 12:00 noon
+                        calendar.set(Calendar.HOUR_OF_DAY, 12);
                         break;
 
                     //Evening is Selected
                     case 2:
-                        Toast.makeText(parent.getContext(), "Spinner item 3!", Toast.LENGTH_SHORT).show();
+
+                        //sets to 5:00 pm
+                        calendar.set(Calendar.HOUR_OF_DAY, 17);
                         break;
 
                     //Night is selectedd
                     case 3:
-                        Toast.makeText(parent.getContext(), "Spinner item 4!", Toast.LENGTH_SHORT).show();
+
+                        //sets to 9:00 pm
+                        calendar.set(Calendar.HOUR_OF_DAY, 21);
                         break;
 
                     //Select a time is selected
@@ -173,10 +204,6 @@ public class Notification extends Activity {
 
             }
         });
-
-
-
-
 
     }
 
@@ -205,14 +232,19 @@ public class Notification extends Activity {
             return new DatePickerDialog(getActivity(), this, year, month, day);
         }
 
+
+        /*
+            When use sets date on the fragemet
+         */
         public void onDateSet(DatePicker view, int year, int month, int day) {
-            // Do something with the date chosen by the user
+
             calendar.set(Calendar.YEAR, year);
             calendar.set(Calendar.MONTH, month);
             calendar.set(Calendar.DAY_OF_MONTH, day);
 
         }
     }
+
 
 
 
@@ -235,14 +267,56 @@ public class Notification extends Activity {
                     DateFormat.is24HourFormat(getActivity()));
         }
 
+
+        /*
+            User selects a time
+         */
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
 
             calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
             calendar.set(Calendar.MINUTE, minute);
 
-
         }
     }
+
+
+
+
+
+    /*
+        Geofening Class
+        ---------------------------------------------------------------------------
+     */
+    private Geofence createGeofence(Address address){
+
+        return new Geofence.Builder()
+                // Set the request ID of the geofence. This is a string to identify this
+                // geofence.
+                .setRequestId(entry.getKey())
+
+                        // Set the circular region of this geofence.
+                .setCircularRegion(
+                        entry.getValue().latitude,
+                        entry.getValue().longitude,
+                        Constants.GEOFENCE_RADIUS_IN_METERS
+                )
+
+                        // Set the expiration duration of the geofence. This geofence gets automatically
+                        // removed after this period of time.
+                .setExpirationDuration(Constants.GEOFENCE_EXPIRATION_IN_MILLISECONDS)
+
+                        // Set the transition types of interest. Alerts are only generated for these
+                        // transition. We track entry and exit transitions in this sample.
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+                        Geofence.GEOFENCE_TRANSITION_EXIT)
+
+                        // Create the geofence.
+                .build();
+
+    }
+
+
+
 
 
 
---
+++ app/src/main/java/codefactory/esy2shop/helpers/GeofencingConstants.java
@@ -0,0 +1,16 @@
+package codefactory.esy2shop.helpers;
+
+/**
+ * Created by James on 11/18/2015.
+ */
+public final class GeofencingConstants {
+
+        private GeofencingConstants() {
+        }
+
+        public static final float GEOFENCE_RADIUS_IN_METERS = 1000; /// 1 kilometer
+
+
+
+
+}
